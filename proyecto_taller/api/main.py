1328d6173af49a1d112bf606aa4fd88e7e904220
LuisAg1256 <laaguilar21@utpl.edu.ec>
2026-04-21 12:28:32 -0500

    Actualizando main.py para usar configuracion externa0

--- proyecto_taller/api/main.py
+++ proyecto_taller/api/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 import psycopg2
 from config import DB_CONFIG 
@@ -16,18 +15,5 @@ def empleados():
     datos = cur.fetchall()
     cur.close()
     conn.close()
-    return [{"nombre": d[0], "departamento": d[1], "salario": d[2]} for d in datos]    user="apiuser",
-    password="1234"
-)
-
-@app.get("/empleados")
-def empleados():
-    conn = conectar()
-    cur = conn.cursor()
-    cur.execute("SELECT nombre, departamento, salario FROM empleados")
-    datos = cur.fetchall()
-    conn.close()
-
-    return [{"nombre":d[0],"departamento":d[1],"salario":d[2]} for d in datos]
-
+    return [{"nombre": d[0], "departamento": d[1], "salario": d[2]} for d in datos]
 
